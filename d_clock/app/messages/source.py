db216fab69ae61c4569d8e162c326f6125810431
Brett Patterson <brett.patterson0@gmail.com>
2015-01-10 16:03:56 -0600

    Implement beginnings of WebSocketSource

--- d_clock/app/messages/source.py
+++ d_clock/app/messages/source.py
@@ -1,7 +1,35 @@
 import json
 import os
 
+import websocket
+
 from message import Message
+
+
+def load_messages_from_json(data):
+    """ Parse messages from a string of JSON data.
+
+    Parameters:
+    -----------
+    data : str
+        The data to be parsed.
+
+    Returns:
+    --------
+    A list of parsed Message objects.
+
+    """
+    result = []
+    try:
+        message_json = json.loads(f.read())
+
+        for data in message_json:
+            result.append(Message.unpack(data))
+
+        return result
+
+    except ValueError:
+        return result
 
 
 class Source(object):
@@ -41,15 +69,46 @@ class FileSource(Source):
         if not os.path.isfile(self.filename):
             return []
 
-        result = []
         with open(self.filename, 'r') as f:
-            try:
-                message_json = json.loads(f.read())
+            return load_messages_from_json(f.read())
 
-                for data in message_json:
-                    result.append(Message.unpack(data))
 
-                return result
+class WebSocketSource(Source):
+    """ A message source that connects to a web socket and listens for
+    messages.
 
-            except ValueError:
-                return result
+    """
+    def __init__(self, host):
+        """ Initialize the WebSocketSource.
+
+        Parameters:
+        -----------
+        host : str
+            The hostname for the web socket server.
+
+        """
+        self._messages = []
+
+        self._host = host
+        self._ws = websocket.WebSocketApp(self._host, on_message=self._receive)
+        self._ws.run_forever()
+
+    def _receive(self, ws, data):
+        """ Receive a message from the websocket server.
+
+        Paramters:
+        ----------
+        ws : WebSocketApp
+            The web socket connection object.
+
+        data : str
+            The message sent from the server.
+
+        """
+        self._messages.extend(load_messages_from_json(data))
+
+    def messages(self):
+        """ Return a list of Message objects received from the server.
+
+        """
+        return self._messages
