c074f5329bbd0061c6abea0a142f71d08a4d559b
Brett Patterson <brett.patterson@rice.edu>
2015-03-12 03:29:20 -0500

    Add basic page for dashboard

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,15 +1,33 @@
 var moment = require('moment');
 var passport = require('passport');
 
+var requireUser = function(req, res, next) {
+    if (req.user !== undefined)
+        next();
+    else
+        res.redirect('/sign-in?nextPage=' + req.path);
+};
+
 var routes = function(app) {
+    app.use(function(req, res, next) {
+        // Inject the user object into the locals of the response.
+        if (req.user !== undefined)
+            res.locals.user = req.user;
+        next();
+    });
+
     app.get('/', function(req, res) {
-        return res.render('index', {user: req.user});
+        return res.render('index');
     });
 
     app.get('/sign-in/', function(req, res) {
-        if (req.user)
+        if (req.user !== undefined)
             return res.redirect('/');
-        return res.render('sign-in', { hideUserInfo: true });
+
+        return res.render('sign-in', {
+            hideUserInfo: true,
+            nextPage: req.query.nextPage
+        });
     });
 
     app.get('/sign-out/', function(req, res) {
@@ -18,22 +36,34 @@ var routes = function(app) {
     });
 
     app.get('/register/', function(req, res) {
-        if (req.user)
+        if (req.user != undefined)
             return res.redirect('/');
         return res.render('register', { hideUserInfo: true });
     });
 
-    app.post('/auth-sign-in/', passport.authenticate('local-sign-in', {
-        failureRedirect: '/sign-in/',
-        successRedirect: '/',
-        failureFlash: true
-    }));
+    app.post('/auth-sign-in/', function(req, res, next) {
+        var options = {
+            failureRedirect: '/sign-in/',
+            successRedirect: '/',
+            failureFlash: true
+        };
+
+        var nextPage = req.body.nextPage;
+        if (nextPage !== 'undefined')
+            options.successRedirect = nextPage;
+
+        passport.authenticate('local-sign-in', options)(req, res, next);
+    });
 
     app.post('/auth-register/', passport.authenticate('local-register', {
         failureRedirect: '/register/',
         successRedirect: '/',
         failureFlash: true
     }));
+
+    app.get('/dashboard/', requireUser, function(req, res) {
+        return res.render('dashboard');
+    })
 
     app.ws('/', function(ws, req) {
         ws.send(JSON.stringify([{
